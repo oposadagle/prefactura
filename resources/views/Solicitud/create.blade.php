b6e2654b903721a6f4401897985d947e5b3a7c0f
oposadagle <oscar.posada@glecolombia.com>
2026-04-17 13:42:16 -0500

    Ajuste ejecutivo

--- resources/views/Solicitud/create.blade.php
+++ resources/views/Solicitud/create.blade.php
@@ -57,7 +57,16 @@
     <div class="col-sm-12">
         <div class="card">
             <div class="card-header d-flex m-2 py-3">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0" /><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" /><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.50959 2C4.01895 2 1.99988 4.01907 1.99988 6.50971V17.4903C1.99988 19.9809 4.01895 22 6.50959 22H11.8499C12.3191 22 12.6994 21.6197 12.6994 21.1505C12.6994 20.6813 12.3191 20.301 11.8499 20.301H6.50959C4.9573 20.301 3.69892 19.0426 3.69892 17.4903V6.50971C3.69892 4.95742 4.9573 3.69904 6.50959 3.69904H17.1902C18.7425 3.69904 20.0008 4.95742 20.0008 6.50971V12C20.0008 12.4692 20.3812 12.8495 20.8504 12.8495C21.3195 12.8495 21.6999 12.4692 21.6999 12V6.50971C21.6999 4.01907 19.6808 2 17.1902 2H6.50959ZM7.00245 11.1503C6.53327 11.1503 6.15293 11.5306 6.15293 11.9998C6.15293 12.469 6.53327 12.8493 7.00245 12.8493H10.0016C10.4708 12.8493 10.8511 12.469 10.8511 11.9998C10.8511 11.5306 10.4708 11.1503 10.0016 11.1503H7.00245ZM6.15293 8.00201C6.15293 7.53283 6.53327 7.15248 7.00245 7.15248H12.9991C13.4683 7.15248 13.8486 7.53283 13.8486 8.00201C13.8486 8.47119 13.4683 8.85153 12.9991 8.85153H7.00245C6.53327 8.85153 6.15293 8.47119 6.15293 8.00201ZM19.4325 13.2912C18.9054 12.7553 18.0508 12.7553 17.5237 13.2912L13.7673 17.1101C13.4779 17.4044 13.3355 17.8162 13.38 18.2297L13.5949 20.2252C13.6508 20.7447 14.0542 21.1549 14.5652 21.2117L16.528 21.4301C16.9348 21.4754 17.3398 21.3306 17.6292 21.0364L21.3856 17.2174C21.9126 16.6815 21.9126 15.8127 21.3856 15.2768L19.4325 13.2912ZM15.0725 18.2059L18.4781 14.7435L19.9571 16.2471L16.5515 19.7095L15.2184 19.5612L15.0725 18.2059Z" fill="#FF2029" /></g></svg>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+                    <g id="SVGRepo_iconCarrier">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M6.50959 2C4.01895 2 1.99988 4.01907 1.99988 6.50971V17.4903C1.99988 19.9809 4.01895 22 6.50959 22H11.8499C12.3191 22 12.6994 21.6197 12.6994 21.1505C12.6994 20.6813 12.3191 20.301 11.8499 20.301H6.50959C4.9573 20.301 3.69892 19.0426 3.69892 17.4903V6.50971C3.69892 4.95742 4.9573 3.69904 6.50959 3.69904H17.1902C18.7425 3.69904 20.0008 4.95742 20.0008 6.50971V12C20.0008 12.4692 20.3812 12.8495 20.8504 12.8495C21.3195 12.8495 21.6999 12.4692 21.6999 12V6.50971C21.6999 4.01907 19.6808 2 17.1902 2H6.50959ZM7.00245 11.1503C6.53327 11.1503 6.15293 11.5306 6.15293 11.9998C6.15293 12.469 6.53327 12.8493 7.00245 12.8493H10.0016C10.4708 12.8493 10.8511 12.469 10.8511 11.9998C10.8511 11.5306 10.4708 11.1503 10.0016 11.1503H7.00245ZM6.15293 8.00201C6.15293 7.53283 6.53327 7.15248 7.00245 7.15248H12.9991C13.4683 7.15248 13.8486 7.53283 13.8486 8.00201C13.8486 8.47119 13.4683 8.85153 12.9991 8.85153H7.00245C6.53327 8.85153 6.15293 8.47119 6.15293 8.00201ZM19.4325 13.2912C18.9054 12.7553 18.0508 12.7553 17.5237 13.2912L13.7673 17.1101C13.4779 17.4044 13.3355 17.8162 13.38 18.2297L13.5949 20.2252C13.6508 20.7447 14.0542 21.1549 14.5652 21.2117L16.528 21.4301C16.9348 21.4754 17.3398 21.3306 17.6292 21.0364L21.3856 17.2174C21.9126 16.6815 21.9126 15.8127 21.3856 15.2768L19.4325 13.2912ZM15.0725 18.2059L18.4781 14.7435L19.9571 16.2471L16.5515 19.7095L15.2184 19.5612L15.0725 18.2059Z"
+                            fill="#FF2029" />
+                    </g>
+                </svg>
                 <h4 class="card-title" style="margin-left: 10px;">CREAR SOLICITUD DE SERVICIO</h4>
             </div><!--end card-header-->
             <div class="card-body">
@@ -108,8 +117,8 @@
 
                             <div class="col-lg-2 col-md-6">
                                 <div class="form-floating mb-3">
-                                    <input style="text-align: right" type="text" class="form-control" id="timepikcr"
-                                        name="hora_descargue" readonly autocomplete="off">
+                                    <input style="text-align: right" type="text" class="form-control"
+                                        id="timepikcr" name="hora_descargue" readonly autocomplete="off">
                                     <label for="hora_descargue">Hora de descargue</label>
                                 </div>
                             </div>
@@ -160,17 +169,17 @@
                                     </select>
                                     <label for="destino">Destino</label>
                                 </div>
-                            </div>                            
+                            </div>
 
-                            <div class="col-lg-2 col-md-3">                                
+                            <div class="col-lg-2 col-md-3">
                                 <div class="form-floating mb-3">
-                                    <select class="form-select" id="tipo_trayecto" name="tipo_trayecto" autocomplete="off"
-                                        aria-label="Floating label select example">
+                                    <select class="form-select" id="tipo_trayecto" name="tipo_trayecto"
+                                        autocomplete="off" aria-label="Floating label select example">
                                         <option selected disabled>Seleccionar</option>
                                         <option>URBANO</option>
-                                        <option>NACIONAL</option>                                        
-                                        <option>MUNICIPAL</option>                                        
-                                        <option>TRAYECTO ESPECIAL</option>                                        
+                                        <option>NACIONAL</option>
+                                        <option>MUNICIPAL</option>
+                                        <option>TRAYECTO ESPECIAL</option>
                                     </select>
                                     <label for="ejecutivo">Tipo de trayecto</label>
                                 </div>
@@ -197,67 +206,70 @@
                     <div class="container">
                         <div class="row gx-1 justify-content-between">
 
-                            <div class="col-lg-2 col-md-3">                                
+                            <div class="col-lg-2 col-md-3">
                                 <div class="form-floating mb-3">
                                     <select class="form-select" id="carroceria" name="carroceria" autocomplete="off"
                                         aria-label="Floating label select example">
                                         <option selected disabled>Seleccionar</option>
-                                        <option>CARROZADO</option>                                        
+                                        <option>CARROZADO</option>
                                         <option>FURGONADO</option>
-                                        <option>MOTO</option>                   
+                                        <option>MOTO</option>
                                         <option>PLANCHON</option>
                                         <option>PORTACONTENEDOR</option>
                                     </select>
                                     <label for="ejecutivo">Tipo de carrocería</label>
                                 </div>
                             </div>
-                            
+
                             <div class="col-lg-2 col-md-12">
                                 <div class="form-floating mb-3">
-                                    <input type="text" class="form-control" id="documento_cliente" name="documento_cliente">
+                                    <input type="text" class="form-control" id="documento_cliente"
+                                        name="documento_cliente">
                                     <label for="cliente">Documento cliente</label>
                                 </div>
                             </div>
 
                             <div class="col-lg-2 col-md-6">
                                 <div class="form-floating mb-3">
-                                    <input type="text" class="form-control" id="destinatario" name="destinatario">
-                                    <label for="cliente">Destinatario</label>                                    
+                                    <input type="text" class="form-control" id="destinatario"
+                                        name="destinatario">
+                                    <label for="cliente">Destinatario</label>
                                 </div>
                             </div>
 
                             <div class="col-lg-2 col-md-6">
                                 <div class="form-floating mb-3">
                                     <input type="text" class="form-control" id="direccion" name="direccion">
-                                    <label for="cliente">Direccion</label>                                    
-                                </div>
-                            </div>                            
-
-                            <div class="col-lg-2 col-md-3">                                
-                                <div class="form-floating mb-3">
-                                    <input type="text" class="form-control" id="piezas" name="piezas">
-                                    <label for="cliente">Piezas</label>                                    
+                                    <label for="cliente">Direccion</label>
                                 </div>
                             </div>
 
-                            
+                            <div class="col-lg-2 col-md-3">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="piezas" name="piezas">
+                                    <label for="cliente">Piezas</label>
+                                </div>
+                            </div>
+
+
 
                         </div>
                     </div>
 
                     <div class="container">
-                        <div class="row gx-1 justify-content-between py-4">                            
+                        <div class="row gx-1 justify-content-between py-4">
 
                             <div class="col-lg-2 col-md-6">
                                 <div class="form-floating mb-3">
                                     <input type="text" class="form-control" id="peso" name="peso">
-                                    <label for="cliente">Peso (Kg)</label>                                    
+                                    <label for="cliente">Peso (Kg)</label>
                                 </div>
                             </div>
-                           
+
                             <div class="col-lg-2 col-md-6">
                                 <div class="form-floating mb-3">
-                                    <input type="text" class="form-control" id="valor_declarado" name="valor_declarado">
+                                    <input type="text" class="form-control" id="valor_declarado"
+                                        name="valor_declarado">
                                     <label for="ejecutivo">$ Valor declarado</label>
                                 </div>
                             </div>
@@ -266,7 +278,6 @@
                                     <select class="form-select" id="ejecutivo" name="ejecutivo" autocomplete="off"
                                         aria-label="Floating label select example">
                                         <option selected disabled>Seleccionar</option>
-                                        <option>Daniela Prieto</option>
                                         <option>Derney Santana</option>
                                         <option>GLE</option>
                                         <option>Jessica Motta</option>
@@ -274,6 +285,7 @@
                                         <option>Maicol Palomino</option>
                                         <option>Martha Duarte</option>
                                         <option>Paulina Lucero</option>
+                                        <option>Ricardo Trujillo</option>
                                     </select>
                                     <label for="ejecutivo">Ejecutivo de cuenta</label>
                                 </div>
@@ -286,7 +298,7 @@
                                     <label for="fecha_solicitud">Observaciones</label>
                                 </div>
                             </div>
-                            
+
                             <div class="col-lg-12 col-md-6 pt-4">
                                 <div class="button-items" style="text-align: right">
                                     <button type="submit" class="btn btn-outline-primary py-2"><i
@@ -336,13 +348,13 @@
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         const form = document.getElementById('solicitudForm');
-        
+
         if (form) {
             form.addEventListener('submit', async function(e) {
                 e.preventDefault();
 
                 const formData = new FormData(this);
-                
+
                 try {
                     const response = await fetch('{{ url('/solicitud') }}', {
                         method: 'POST',
@@ -364,11 +376,12 @@
                         });
                     } else {
                         const contentType = response.headers.get('content-type');
-                        
-                        if (response.status === 422 && contentType && contentType.includes('application/json')) {
+
+                        if (response.status === 422 && contentType && contentType.includes(
+                                'application/json')) {
                             const errors = await response.json();
                             let errorMessages = '';
-                            
+
                             document.querySelectorAll('.form-control, .form-select').forEach(el => {
                                 el.classList.remove('is-invalid');
                             });
@@ -376,19 +389,22 @@
                             if (errors.errors) {
                                 Object.keys(errors.errors).forEach(field => {
                                     const messages = errors.errors[field];
-                                    const fieldElement = document.querySelector(`[name="${field}"]`);
-                                    
+                                    const fieldElement = document.querySelector(
+                                        `[name="${field}"]`);
+
                                     if (fieldElement) {
                                         fieldElement.classList.add('is-invalid');
                                     }
-                                    
-                                    errorMessages += '<li>' + messages.join('</li><li>') + '</li>';
+
+                                    errorMessages += '<li>' + messages.join('</li><li>') +
+                                        '</li>';
                                 });
                             }
 
                             Swal.fire({
                                 title: "¡Validación fallida!",
-                                html: '<div style="text-align: left;"><ul style="list-style: disc; margin-left: 20px;">' + errorMessages + '</ul></div>',
+                                html: '<div style="text-align: left;"><ul style="list-style: disc; margin-left: 20px;">' +
+                                    errorMessages + '</ul></div>',
                                 icon: "error",
                                 confirmButtonText: "Aceptar",
                                 allowOutsideClick: false,
@@ -423,7 +439,7 @@
             });
         }
 
-        document.getElementById('valor_declarado').addEventListener('input', function (e) {
+        document.getElementById('valor_declarado').addEventListener('input', function(e) {
             let value = e.target.value.replace(/\D/g, '');
             value = Number(value).toLocaleString('es');
             e.target.value = value;
