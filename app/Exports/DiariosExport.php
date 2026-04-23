2bdbcefe03a394195c8a9d992ef5f1d140763f14
oposadagle <oscar.posada@glecolombia.com>
2026-04-23 09:04:10 -0500

    Ajuste excel anticipos

--- app/Exports/DiariosExport.php
+++ app/Exports/DiariosExport.php
@@ -12,15 +12,15 @@ class DiariosExport implements FromCollection, WithHeadings, WithMapping
 {
     public function collection()
     {
-        $incluidos = (['PM. ANTICIPAR', 'AM. ANTICIPAR', 'CONTADO', 'CONTADO AM.', 'CONTADO PM.']);
+        $incluidos = (['PM. ANTICIPAR', 'AM. ANTICIPAR', 'CONTADO', 'CONTADO AM.', 'CONTADO PM.', 'ANTICIPO NOCHE']);
         $excluidos = (['Servicio finalizado', 'Servicio cancelado']);
         $startOfLastMonth = Carbon::now()->subMonth()->startOfMonth()->toDateString(); // Inicio del mes anterior
         $endOfCurrentMonth = Carbon::now()->endOfMonth()->toDateString(); // Fin del mes actual
 
         return DB::table('peticiones')
             ->select(
-                'fecha_cargue', 'razon', 'paytype', 'cliente', 'origen', 'destino', 'placa', 
-                'conductor', 'asociado', 'cedula_asociado', 'pagarsaldo', 'cedula_saldo', 
+                'razon', 'fecha_cargue', 'paytype', 'nit', 'cliente', 'origen', 'destino', 'placa', 
+                'conductor', 'pagant', 'cpagant', 'tpagant', 'pagsal', 'cpagsal', 'pagcon', 'cpagcon',
                 'tipo_vehiculo', 'costo', 'costo_tiposerv', 'anticipo', 'pago_completo', 
                 'centro_costo', 'reteica', 'retefuente', 'seguro', 'valor_a_pagar'
             )
@@ -28,7 +28,7 @@ class DiariosExport implements FromCollection, WithHeadings, WithMapping
             ->where('confirmado', 'NO')
             ->whereNotNull('razon')
             ->whereIn('paytype', $incluidos)
-            ->whereNotIn('state', $excluidos) // Corregido 'states' a 'state'
+            ->whereNotIn('states', $excluidos)
             ->whereBetween('fecha_cargue', [$startOfLastMonth, $endOfCurrentMonth])
             ->orderBy('fecha_cargue', 'desc')
             ->get();
@@ -37,18 +37,22 @@ class DiariosExport implements FromCollection, WithHeadings, WithMapping
     public function map($record): array
     {
         return [
-            $record->fecha_cargue,
             $record->razon,
+            $record->fecha_cargue,
             $record->paytype,
+            $record->nit,
             $record->cliente,
             strtoupper($record->origen),
             strtoupper($record->destino),
             $record->placa,
             strtoupper($record->conductor),
-            strtoupper($record->asociado),
-            $record->cedula_asociado,
-            strtoupper($record->pagarsaldo),
-            $record->cedula_saldo,
+            $record->pagant,
+            $record->cpagant,
+            $record->tpagant,
+            $record->pagsal,
+            $record->cpagsal,
+            $record->pagcon,
+            $record->cpagcon,
             $record->tipo_vehiculo,
             $record->costo,
             $record->costo_tiposerv,
@@ -65,10 +69,10 @@ class DiariosExport implements FromCollection, WithHeadings, WithMapping
     public function headings(): array
     {
         return [
-            'CARGUE', 'MANIFIESTO', 'CONDICION DE PAGO', 'CLIENTE', 'ORIGEN', 'DESTINO', 'PLACA',
-            'CONDUCTOR', 'PAGAR ANTICIPO A', 'CEDULA ANTICIPO', 'PAGAR SALDO A', 'CEDULA SALDO', 
-            'TIPO VEHICULO', 'COSTO', 'EXTRA', 'ANTICIPO', 'PAGO COMPLETO', 'CENTRO DE COSTO', 
-            'RETEICA', 'RETEFUENTE', 'SEGURO', 'VALOR A PAGAR'
+            'MANIFIESTO', 'CARGUE', 'CONDICION DE PAGO', 'NIT', 'CLIENTE', 'ORIGEN', 'DESTINO', 'PLACA',
+            'CONDUCTOR', 'PAGAR ANTICIPO A', 'CEDULA ANTICIPO', 'TELEFONO ANTICIPO', 'PAGAR SALDO A', 'CEDULA SALDO', 
+            'PAGAR CONTADO A', 'CEDULA CONTADO', 'TIPO VEHICULO', 'COSTO', 'EXTRA', 'ANTICIPO', 'PAGO COMPLETO', 
+            'CENTRO DE COSTO', 'RETEICA', 'RETEFUENTE', 'SEGURO', 'VALOR A PAGAR'
         ];
     }
 }
